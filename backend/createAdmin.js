3dcd009e72e1af4e1d91d9288c986cbd4971121f
SANKALP DESHPANDE <sankalp7456@gmail.com>
2025-08-17 04:57:03 +0530

    Working on Login Part

--- backend/createAdmin.js
+++ backend/createAdmin.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const Admin = require("./models/Admin");
 
 const MONGO_URI =
-  "mongodb+srv://admin:admin%402025@cluster0.qapk6t7.mongodb.net/collegeDB?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin%402025@cluster0.qapk6t7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 (async () => {
   try {
